7c97763aa9f488b810d27c818fc4735d1e9375fe
Patrick Leung <pleung1987@gmail.com>
2017-02-25 05:46:57 +0800

    1st Python Exam changed settings.py

--- LoginAndRegistration/settings.py
+++ LoginAndRegistration/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$en4*c+9vst=dohd(=dffm)2b$4#96e#9u^jk)xuyyxi#+m!@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['54.186.189.30']
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+ALLOWED_HOSTS = ['54.186.189.30']
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Application definition
 
